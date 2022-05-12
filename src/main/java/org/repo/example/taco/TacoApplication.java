cedd333d51bc80399d348f723107aa8b28a17805
Dobrosław <twojacksdob@gmail.com>
2022-05-12 11:19:21 +0200

    initial commit

---
+++ src/main/java/org/repo/example/taco/TacoApplication.java
@@ -0,0 +1,14 @@
+package org.repo.example.taco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(TacoApplication.class, args);
+	}
+
+}
